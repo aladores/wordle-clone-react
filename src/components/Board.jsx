2fc66fdc1ca722b6f5b66ee80cd933c93de0d6ff
angelolad <angelo.ladores@outlook.com>
2023-04-27 23:06:38 -0400

    complete inital setup

---
+++ src/components/Board.jsx
@@ -0,0 +1,11 @@
+import './Board.css'
+
+function Board() {
+  return (
+    <div className="board">
+      <p>board</p>
+    </div>
+  )
+}
+
+export default Board
---
+++ src/components/Keyboard.jsx
@@ -0,0 +1,11 @@
+import './Keyboard.css'
+
+function Keyboard() {
+  return (
+    <div className="keyboard">
+      <p>keyboard</p>
+    </div>
+  )
+}
+
+export default Keyboard
