506260993f8312917a1101710bfb3ecc889657b7
angelolad <angelo.ladores@outlook.com>
2023-04-28 04:49:15 -0400

    refactor keyboard elements

--- src/components/keyboard/KeyboardRow.jsx
+++ src/components/keyboard/KeyboardRow.jsx
@@ -3,27 +3,19 @@ import './KeyboardRow.css'
 
 
 function KeyboardRow(props) {
-  // eslint-disable-next-line react/prop-types
-  const buttons = [];
-  function createButtons(){
-    for (let i=0;i<props.row.length; i++){
-      if (props.row[i].length >1){
-        if (props.row[i] === "ENTER"){
-          buttons.push(<button key={props.row[i]} className="keyboard-button keyboard-special" value={props.row[i]} onClick={props.handleSubmit}>{props.row[i]}</button>);
-        }
-        else {
-          buttons.push(<button key={props.row[i]} className="keyboard-button keyboard-special" value={props.row[i]} onClick={props.handleDelete}>{props.row[i]}</button>);
-        }
-      }
-      else{
-        buttons.push(<button key={props.row[i]} className="keyboard-button" value={props.row[i]} onClick={props.handleClick}>{props.row[i]}</button>);
-      }
+  const buttonElements = props.row.map((item)=>{
+    let newOnClick = props.handleClick;
+    let newClassName = "keyboard-button"
+    if (item.length>1){
+      newOnClick = (item === "ENTER" ? props.handleSubmit : props.handleDelete);
+      newClassName += " keyboard-special"
     }
-  }
-  createButtons();
+    return <button key={item} className={newClassName} value={item} onClick={newOnClick}> {item} </button>
+  })
+  
   return (
     <div className="keyboardRow">
-      {buttons}
+      {buttonElements}
     </div>
   )
 }
