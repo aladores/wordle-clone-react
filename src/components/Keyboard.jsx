a71a701b98bc9a572e06e677e998051018d47096
angelolad <angelo.ladores@outlook.com>
2023-04-27 23:35:59 -0400

    added keyboard

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,9 +1,25 @@
+import KeyboardRow from './KeyboardRow'
 import './Keyboard.css'
 
 function Keyboard() {
+
+  const row1= ["Q","W","E","R","T","Y","U","I","O","P"];
+  const row2= ["A","S","D","F","G","H","J","K","L"];
+  const row3= ["ENTER","Z","X","C","V","B","N","M","BACK"];
+
+  // const keyboardRows=()=>{
+  //     for (let i = 0; i < 3; i++){
+        
+  //     }
+  // }
   return (
     <div className="keyboard">
-      <p>keyboard</p>
+      <KeyboardRow
+        row={row1}/>
+      <KeyboardRow
+        row={row2}/>
+      <KeyboardRow
+        row={row3}/>
     </div>
   )
 }
